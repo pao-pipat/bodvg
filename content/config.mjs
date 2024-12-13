d28f9d2dffe56cd094d4698111738db287853e35
Pao Pipatpadungsin <95496184+pao-pipat@users.noreply.github.com>
2024-12-13 15:52:54 +0000

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -2,4 +2,13 @@
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
-export default {};
+const config = {
+  title: "BoDV Glycoprotein",
+  description: "This is a blog containing information on Bornavirus Disease Virus Glycoprotein project including background, plans, and protocols.",
+  author: "Nichakorn Pipatpadungsin (Pao)",
+  domain: "https://bodvg.vercel.app",
+  // links to the pages you want to link to in the navbar and in the footer
+  navLinks: [{ href: "/about", name: "About" }],
+};
+
+export default config;
